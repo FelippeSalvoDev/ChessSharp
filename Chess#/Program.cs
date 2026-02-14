1f5273a16ed6c51058c588d4a95749df1dcad9a2
FelippeSalvoDev <felippesalvo.dev@gmail.com>
2026-02-14 18:12:16 -0300

    feat: corrige nomeclatura de alguns metodos

--- Chess#/Program.cs
+++ Chess#/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Tabuleiro;
 
-
 namespace ChessSharp
 {
     class Program
@@ -8,6 +7,11 @@ namespace ChessSharp
         private static void Main(string[] args)
         {
             TabuleiroJogo tab = new TabuleiroJogo(8, 8);
+
+            tab.ColocarPeca("T", new Posicao(0, 0));
+            tab.ColocarPeca("T", new Posicao(1, 3));
+            tab.ColocarPeca("T", new Posicao(2, 4));
+
 
             Tela.ImprimirTabuleiro(tab);
 
